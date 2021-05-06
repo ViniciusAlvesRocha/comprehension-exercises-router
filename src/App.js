e2d81784a116576b0caec7e617b59ceb85dc9c84
viniciustec <vinialvesrocha@gmail.com>
2021-05-06 16:44:35 -0300

    4-Adicione uma rota que mapeie o caminho de URL /about para o componente About. Acesse http://localhost:3000/about para ver se o componente About foi renderizado!

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 
 import Home from './components/Home';
+import About from './components/About'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path='/'>
+        <Route exact path='/'>
           <Home />
         </Route>
-       <Link to='/'>Home</Link>
-       <br/>
-       <Link to='/about'>About</Link>
-       <br/>
-       <Link to='/users'>Users</Link>
+
+        <Route exact path='/about'>
+          <About />
+        </Route>
+
+        <Link to='/'>Home</Link>
+        <br/>
+        <Link to='/about'>About</Link>
+        <br/>
+        <Link to='/users'>Users</Link>
       </BrowserRouter>
     );
   }
